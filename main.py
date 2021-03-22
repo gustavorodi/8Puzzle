7840ab3df595f5d6a8f4e11d3969ce84e03e3be5
Henrique Hänsch Stepic <38055978+HenriqueHanschStepic@users.noreply.github.com>
2021-03-22 19:38:29 -0300

    Grid 8puzzle
    Henrique Hänsch Stepic

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 
+class GridBox:
+
+  def _init_(self):
+    self.lastNumber = None
+    self.listParams = [1,2,3,4,5,6,7,8, 9]
+            
+  def setGridBox(self):
+    self.listParams = random.sample(self.listParams, 9)
+    indexGrid = 0
+    indexList = 0
+
+
+    while indexGrid < 3:
+      print(f"{self.listParams[0+indexList]}|{self.listParams[1+indexList]}|{self.listParams[2+indexList]}")
+
+      if indexGrid < 2:
+        print("- - -")
+
+      indexGrid += 1
+      indexList += 3
+
+
+
+    indexGrid = 0
+    indexList = 0
+
+    self.organize()
+    print("")
+
+    while indexGrid < 3:
+      print(f"{self.listParams[0+indexList]}|{self.listParams[1+indexList]}|{self.listParams[2+indexList]}")
+
+      if indexGrid < 2:
+        print("- - -")
+      
+      indexGrid += 1
+      indexList += 3
